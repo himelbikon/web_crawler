826df8155037b74fa3aedbc8b89defb826e79012
himelbikon <skhimelbikon@gmail.com>
2020-10-12 23:36:59 +0600

    verson 2

--- web_crawler.py
+++ web_crawler.py
@@ -2,6 +2,9 @@ import requests, bs4, re, urllib.parse
 
 target_url = input('Enter URL: ')
 main_list = []
+tem_list = []
+num = 1
+link_limit = 500
 
 def response(url):
 	try:
@@ -18,6 +21,7 @@ def link_filter(list_1):
 	return list_2
 
 def main(url):
+	global num
 	result = response(url)
 
 	if result[0] == 'posetive':
@@ -27,9 +31,15 @@ def main(url):
 		for x in a_list:
 			if target_url in x and not x in main_list:
 				main_list.append(x)
+				tem_list.append(x)
 				print('[%d]' % len(main_list), x)
-				with open('links.txt', 'a') as f:
-					f.write(x + '\n')
+				if len(tem_list) >= link_limit:
+					f = open('links_%d.txt' % num, 'w')
+					for y in tem_list:
+						f.write(y + '\n')
+					f.close()
+					num += 1
+					tem_list.clear()
 				main(x)
 
 main(target_url)
